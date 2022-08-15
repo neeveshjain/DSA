e3e8fa6d2b02c90deaf2f593afa814d330c8de9a
neeveshjain <neeveshjain27@gmail.com>
2022-08-15 20:01:48 +0530

    new file

---
+++ StructureasParameter.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int area(struct rectangle &r1){
+    r1.length++;
+    return r1.length*r1.breadth;
+}
+
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+void changeLeangth(struct rectangle *p,int l){
+    P->length=l;   
+}
+int main(){
+    struct rectangle r;
+    r.length=10;
+    r.breadth=5;
+    cout<<area<<endl;
+    changeLeangth(&r,20);
+    return 0;
+}
